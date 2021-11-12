61245686ad3b4b4c41eec789eff1e54f94ce1643
Gene Cook <genec@genes-cube>
2021-11-12 13:10:16 -0600

    use coordinate transforms to simplify/cleanup code

--- main.c
+++ main.c
@@ -46,12 +46,12 @@ int main(void)
       line_sweep(RIGHT_UPPER_CORNER,RED,line_style,dot_pixel);
       line_sweep(RIGHT_UPPER_CORNER,BLACK,line_style,dot_pixel);
       
-      line_sweep(LEFT_LOWER_CORNER,GREEN,line_style,dot_pixel);
-      line_sweep(LEFT_LOWER_CORNER,BLACK,line_style,dot_pixel);
-      
       line_sweep(RIGHT_LOWER_CORNER,BLUE,line_style,dot_pixel);
       line_sweep(RIGHT_LOWER_CORNER,BLACK,line_style,dot_pixel);
 
+      line_sweep(LEFT_LOWER_CORNER,GREEN,line_style,dot_pixel);
+      line_sweep(LEFT_LOWER_CORNER,BLACK,line_style,dot_pixel);
+      
 	//Driver_Delay_ms(1000);
     }
 
@@ -63,55 +63,39 @@ void line_sweep_inner(int x_start,int y_start,int x_end,int y_end,
   GUI_DrawLine(x_start,y_start,x_end,y_end,color,line_style,dot_pixel);	
 }
 
+// # of pixels in X, Y direction:
 #define X_PIXELS_MAX 240
 #define Y_PIXELS_MAX 320
 
-void line_sweep(DISPLAY_CORNERS corner,COLOR color,LINE_STYLE line_style,DOT_PIXEL dot_pixel) {
-  
-  int x_min    = 0;
-  int y_min    = 0;
-  int x_max    = X_PIXELS_MAX;
-  int y_max    = Y_PIXELS_MAX;
-  int x_incr   = 20;
-  int y_incr   = 20;
+// last x, y addressible pixel:
+#define LAST_X_PIXEL X_PIXELS_MAX - 1 
+#define LAST_Y_PIXEL Y_PIXELS_MAX - 1
 
+// DCT - transform 'logical' display coordinates (coordinates relative to corner of display)
+//       into physical display coordinates (relative to upper left hand of pico lcd display)
+
+void DCT(int *px, int *py, int lx, int ly, DISPLAY_CORNERS corner) {
   switch(corner) {
-    case 0: // origin is upper left hand corner...
-
-            for (int x = x_min; x < x_max; x += x_incr) {
-               for (int y = y_min; y < y_max; y += y_incr) {
-		  line_sweep_inner(x_min,y_min,x,y,color,line_style,dot_pixel);	
-               }
-            }
-            break;
-  
-    case 1: // origin is upper right hand corner...
-
-            for (int x = x_max - 1; x >= x_min; x -= x_incr) {
-               for (int y = y_min; y < y_max; y += y_incr) {
-		  line_sweep_inner(x_max - 1,y_min,x,y,color,line_style,dot_pixel);	
-               }
-            }
-            break;
-
-    case 2: // origin  is lower right hand corner...
-
-            for (int x = x_max - 1; x >= x_min; x -= x_incr) {
-	       for (int y = y_max - 1; y >= y_min; y -= y_incr) {
-	          line_sweep_inner(x_max - 1,y_max - 1,x,y,color,line_style,dot_pixel);	
-               }
-            }
-            break;
-
-    case 3: // origin is lower left hand corner...
-
-            for (int x = x_min; x < x_max; x += x_incr) {
-	       for (int y = y_max - 1; y >= y_min; y -= y_incr) {
-  	          line_sweep_inner(x_min,y_min,x,y,color,line_style,dot_pixel);	
-               }
-            }
-            break;
-
+    case LEFT_UPPER_CORNER:  *px = lx;                *py = ly;                break;
+    case RIGHT_UPPER_CORNER: *px = LAST_X_PIXEL - lx; *py = ly;                break;
+    case RIGHT_LOWER_CORNER: *px = LAST_X_PIXEL - lx; *py = LAST_Y_PIXEL - ly; break;
+    case LEFT_LOWER_CORNER:  *px = lx;                *py = LAST_Y_PIXEL - ly; break;
     default: break;
+  }
+}
+
+void line_sweep(DISPLAY_CORNERS corner,COLOR color,LINE_STYLE line_style,DOT_PIXEL dot_pixel) {
+  int x_incr = 20;
+  int y_incr = 20;
+
+  int px_start, py_start;
+  DCT(&px_start,&py_start,0,0,corner);
+
+  for (int x = 0; x < X_PIXELS_MAX; x += x_incr) {
+     for (int y = 0; y < Y_PIXELS_MAX; y += y_incr) {
+        int px_end, py_end;
+        DCT(&px_end,&py_end,x,y,corner);
+	line_sweep_inner(px_start,py_start,px_end,py_end,color,line_style,dot_pixel);	
+     }
   }
 }
